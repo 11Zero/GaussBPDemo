9115c9d2dab7b0d93e4fa270f4d6bc0e00e3d6d7
11Zero <874612467@qq.com>
2017-02-27 00:20:52 +0800

    修改预测函数，添加输出datatable功能

--- ClassForecast/Forecast.cs
+++ ClassForecast/Forecast.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 
 namespace ClassForecast
 {
@@ -19,9 +20,9 @@ namespace ClassForecast
 
         public int input_row = new int();
         public int input_test_row = new int();
-        public int input_col = new int();
-        public int sample_num = new int();//sample_row应该等于input_row
-        public int forecast_num = new int();
+        private int input_col = new int();
+        private int sample_num = new int();//sample_row应该等于input_row
+        //private int forecast_num = new int();
 
 
         public Forecast()
@@ -47,7 +48,7 @@ namespace ClassForecast
             input_test_row = 0;
             input_col = 0;
             sample_num = 0;
-            forecast_num = 0;
+            //forecast_num = 0;
         }
 
         private static double[,] MakeRandDbMat(int row, int col, double min = 0.0, double max = 1.0)
@@ -512,6 +513,220 @@ namespace ClassForecast
             }
             double[] yun = ReserveMaxMin(yuce, outputps);
             return yun;
+        }
+
+        public DataTable MakeForecast(int glag)
+        {
+            //            
+            //% %% 清空环境变量
+            //% clc
+            //% clear
+            //% 
+            //% %% 网络参数配置
+            //% load traffic_flux input output input_test output_test
+            //function wavenn(input,output,input_test,output_test)
+
+            //M=size(input,2); %输入节点个数
+            //N=size(output,2); %输出节点个数
+            if (!IsFilled)
+            {
+                return null;
+            }
+            int M = input.Count;
+            int N = 1;//output.Count
+
+            int n = 6;
+            double lr1 = 0.01;
+            double lr2 = 0.001;
+            int maxgen = 100;
+
+            //n=6; %隐形节点个数
+            //lr1=0.01; %学习概率
+            //lr2=0.001; %学习概率
+            //maxgen=100; %迭代次数
+
+
+            double[,] Wjk = MakeRandDbMat(n, M);
+            double[,] Wjk_1 = Wjk;
+            double[,] Wjk_2 = Wjk;
+
+            double[,] Wij = MakeRandDbMat(N, n);
+            double[,] Wij_1 = Wjk;
+            double[,] Wij_2 = Wjk;
+
+            double[,] a = MakeRandDbMat(1, n);
+            double[,] a_1 = a;
+            double[,] a_2 = a;
+
+            double[,] b = MakeRandDbMat(1, n);
+            double[,] b_1 = b;
+            double[,] b_2 = b;
+            //%权值初始化
+            //Wjk=randn(n,M);Wjk_1=Wjk;Wjk_2=Wjk_1;
+            //Wij=randn(N,n);Wij_1=Wij;Wij_2=Wij_1;
+            //a=randn(1,n);a_1=a;a_2=a_1;
+            //b=randn(1,n);b_1=b;b_2=b_1;
+            double y = 0.0;
+            double[,] net = MakeRandDbMat(1, n, 0, 0);
+            double[,] net_ab = MakeRandDbMat(1, n, 0, 0);
+            //%节点初始化
+            //y=zeros(1,N);
+            //net=zeros(1,n);
+            //net_ab=zeros(1,n);
+            double[,] d_Wjk = MakeRandDbMat(n, M, 0, 0);
+            double[,] d_Wij = MakeRandDbMat(N, n, 0, 0);
+            double[,] d_a = MakeRandDbMat(1, n, 0, 0);
+            double[,] d_b = MakeRandDbMat(1, n, 0, 0);
+            //%权值学习增量初始化
+            //d_Wjk=zeros(n,M);
+            //d_Wij=zeros(N,n);
+            //d_a=zeros(1,n);
+            //d_b=zeros(1,n);
+            double[,] inputps = GetDbMatMaxMin(input);
+            double[,] inputn = DbMapMinMax(input, inputps);
+            double[] outputps = GetDbMatMaxMin(output);
+            double[,] outputn = DbMapMinMax(output, outputps);
+            //%% 输入输出数据归一化
+
+
+            //[inputn,inputps]=mapminmax(input');
+            //[outputn,outputps]=mapminmax(output'); 
+            //inputn=inputn';
+            //outputn=outputn';
+            double[] error = new double[maxgen];
+            for (int i = 0; i < maxgen; i++)
+            {
+                error[i] = 0;
+                for (int kk = 0; kk < input.ElementAt(0).Count; kk++)
+                {
+                    double[] x = new double[input.Count];
+                    double yqw = new double();
+                    for (int j = 0; j < input.Count; j++)
+                    {
+                        x[j] = inputn[kk, j];
+                    }
+                    yqw = outputn[kk, 0];
+                    for (int j = 0; j < n; j++)
+                    {
+                        for (int k = 0; k < M; k++)
+                        {
+                            net[0, j] = net[0, j] + Wjk[j, k] * x[k];
+                            net_ab[0, j] = (net_ab[0, j] - b[0, j]) / a[0, j];
+                        }
+                        double temp = mymorlet(net_ab[0, j]);
+                        for (int k = 0; k < N; k++)
+                        {
+                            y = y + Wij[k, j] * temp;
+                        }
+                    }
+                    error[i] = error[i] + Math.Abs(yqw - y);
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        double temp = mymorlet(net_ab[0, j]);
+                        for (int k = 0; k < N; k++)
+                        {
+                            d_Wij[k, j] = d_Wij[k, j] - (yqw - y) * temp;
+                        }
+                        temp = d_mylorlet(net_ab[0, j]);
+                        for (int k = 0; k < M; k++)
+                        {
+                            for (int l = 0; l < N; l++)
+                            {
+                                d_Wjk[j, k] = d_Wjk[j, k] + (yqw - y) * Wij[l, j];
+                            }
+                            d_Wjk[j, k] = -d_Wjk[j, k] * temp * x[k] / a[0, j];
+                        }
+                        d_b[0, j] = d_b[0, j] + (yqw - y) * Wij[0, j];
+                        d_b[0, j] = d_b[0, j] * temp / a[0, j];
+                        d_a[0, j] = d_a[0, j] + (yqw - y) * Wij[0, j];
+                        d_a[0, j] = d_a[0, j] * temp * ((net[0, j] - b[0, j]) / b[0, j]) / a[0, j];
+                    }
+
+                    //权值参数更新
+                    for (int j = 0; j < N; j++)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            Wij[j, k] = Wij[j, k] - lr1 * d_Wij[j, k];
+                        }
+                    }
+                    for (int j = 0; j < n; j++)
+                    {
+                        for (int k = 0; k < M; k++)
+                        {
+                            Wjk[j, k] = Wjk[j, k] - lr1 * d_Wjk[j, k];
+                        }
+                    }
+                    for (int j = 0; j < 1; j++)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            b[j, k] = b[j, k] - lr2 * d_b[j, k];
+                        }
+                    }
+                    for (int j = 0; j < 1; j++)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            a[j, k] = a[j, k] - lr2 * d_a[j, k];
+                        }
+                    }
+                    d_Wjk = MakeRandDbMat(n, M, 0, 0);
+                    d_Wij = MakeRandDbMat(N, n, 0, 0);
+                    d_a = MakeRandDbMat(1, n, 0, 0);
+                    d_b = MakeRandDbMat(1, n, 0, 0);
+
+                    y = 0;
+                    net = MakeRandDbMat(1, n, 0, 0);
+                    net_ab = MakeRandDbMat(1, n, 0, 0);
+
+                    Wjk_1 = Wjk;
+                    Wjk_2 = Wjk;
+                    Wij_1 = Wij;
+                    Wij_2 = Wij;
+                    a_1 = a;
+                    a_2 = a;
+                    b_1 = b;
+                    b_2 = b;
+                }
+            }
+            double[,] xx = DbMapMinMax(input_test, inputps);
+            double[] yuce = new double[input_test.ElementAt(0).Count];
+            for (int i = 0; i < input_test.ElementAt(0).Count; i++)
+            {
+                double[] xx_test = new double[input_test.ElementAt(0).Count];
+                for (int j = 0; j < input_test.Count; j++)
+                {
+                    xx_test[j] = input_test.ElementAt(j).ElementAt(i);
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < M; k++)
+                    {
+                        net_ab[0, j] = net[0, j] + Wjk[j, k] * xx_test[k];
+                        net_ab[0, j] = (net_ab[0, j] - b[0, j]) / a[0, j];
+                    }
+                    double temp = mymorlet(net_ab[0, j]);
+                    for (int k = 0; k < N; k++)
+                    {
+                        y = y + Wij[k, j] * temp;
+                    }
+                }
+                yuce[i] = y;
+                y = 0.0;
+                net = MakeRandDbMat(1, n, 0, 0);
+                net_ab = MakeRandDbMat(1, n, 0, 0);
+            }
+            double[] yun = ReserveMaxMin(yuce, outputps);
+            DataTable result = new DataTable();
+            result.Columns.Add("时间", typeof(string));
+            result.Columns.Add("数据", typeof(double));
+            for (int i = 0; i < yun.Length; i++)
+            {
+                result.Rows.Add("预测",yun[i]);
+            }
+            return result;
         }
 
     }
